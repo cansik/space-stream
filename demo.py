5675617a1ce60a4b31a9eb6d2cd3934fad1e783a
Florian Bruggisser <florian@bildspur.ch>
2022-02-18 15:33:33 +0100

    implemented 8bit in red channel

--- demo.py
+++ demo.py
@@ -251,7 +251,7 @@ class DemoPipeline(vg.BaseGraph):
         # bit depth 16 bit
         out = np.expand_dims(depth, axis=2)
 
-        r_channel = depth / 256
+        r_channel = out / 256
         g_channel = (out >> 8) & 0xff
         b_channel = out & 0xff
 
