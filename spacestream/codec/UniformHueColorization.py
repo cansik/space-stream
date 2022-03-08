0b61bb08f51b7d10aa7b7db54b18ce9156a2ce90
Florian Bruggisser <florian@bildspur.ch>
2022-03-08 14:30:21 +0100

    fixed uniform hue codec

--- spacestream/codec/UniformHueColorization.py
+++ spacestream/codec/UniformHueColorization.py
@@ -8,58 +8,60 @@ INDEPENDENT_VALUES = 1529
 
 class UniformHueColorization(DepthCodec):
     def encode(self, depth: np.ndarray, d_min: float, d_max: float) -> np.ndarray:
-        result = self._pencode(depth, d_min, d_max)
+        result = self._pencode(depth, d_min * 1000, d_max * 1000)
         return result
 
     @staticmethod
-    @njit
+    @njit(parallel=True)
     def _pencode(depth: np.ndarray, d_min: float, d_max: float) -> np.ndarray:
         h, w = depth.shape[:2]
         result = np.zeros(shape=(h, w, 3), dtype=np.uint8)
 
-        for y in range(h):
-            for x in range(w):
-                d = depth[y, x]
+        for i in prange(w * h):
+            x = i % w
+            y = i // w
 
-                # create rgb
-                r, g, b = 0, 0, 0
+            d = depth[y, x]
 
-                # normalize depth
-                d_norm = ((d - d_min) / (d_max - d_min)) * INDEPENDENT_VALUES
+            # create rgb
+            r, g, b = 0, 0, 0
 
-                # red
-                if 0 <= d_norm <= 255 or 1275 < d_norm <= 1529:
-                    r = 255
-                elif 255 < d_norm <= 510:
-                    r = 255 - d
-                elif 510 < d_norm <= 1020:
-                    r = 0
-                elif 1020 < d_norm <= 1275:
-                    r = d_norm - 1020
+            # normalize depth
+            d_norm = ((d - d_min) / (d_max - d_min)) * INDEPENDENT_VALUES
 
-                # green
-                if 0 < d_norm <= 255:
-                    g = d_norm
-                elif 255 < d_norm <= 510:
-                    g = 255
-                elif 510 < d_norm <= 765:
-                    g = 765 - d_norm
-                elif 765 < d_norm <= 1529:
-                    g = 0
+            # red
+            if 0 <= d_norm <= 255 or 1275 < d_norm <= 1529:
+                r = 255
+            elif 255 < d_norm <= 510:
+                r = 255 - d_norm
+            elif 510 < d_norm <= 1020:
+                r = 0
+            elif 1020 < d_norm <= 1275:
+                r = d_norm - 1020
 
-                # blue
-                if 0 < d_norm <= 765:
-                    b = 0
-                elif 765 < d_norm <= 1020:
-                    b = d_norm - 765
-                elif 1024 < d_norm <= 1275:
-                    b = 255
-                elif 1275 < d_norm <= 1529:
-                    b = 1275 - d_norm
+            # green
+            if 0 < d_norm <= 255:
+                g = d_norm
+            elif 255 < d_norm <= 510:
+                g = 255
+            elif 510 < d_norm <= 765:
+                g = 765 - d_norm
+            elif 765 < d_norm <= 1529:
+                g = 0
 
-                result[y, x, 0] = b
-                result[y, x, 1] = g
-                result[y, x, 2] = r
+            # blue
+            if 0 < d_norm <= 765:
+                b = 0
+            elif 765 < d_norm <= 1020:
+                b = d_norm - 765
+            elif 1024 < d_norm <= 1275:
+                b = 255
+            elif 1275 < d_norm <= 1529:
+                b = 1275 - d_norm
+
+            result[y, x, 0] = b
+            result[y, x, 1] = g
+            result[y, x, 2] = r
 
         return result
 
